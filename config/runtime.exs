a9079fba6dede39f90f0e87c8477c6b9f63ec1f3
Ruben Baeza <ruben44bac@gmail.com>
2024-01-16 00:22:26 -0600

    update runtime

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,7 +48,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "ceduc.gigalixirapp.com"
+  host = System.get_env("PHX_HOST") || "ceduc.org"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :ceduc, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
@@ -63,7 +63,12 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    check_origin: [
+      "//ceduc.org",
+      "//www.ceduc.org",
+      "//ceduc.gigalixirapp.com/"
+    ]
 
   # ## SSL Support
   #
